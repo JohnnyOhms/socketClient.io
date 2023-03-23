8b5d0c720c18cd78903843db926dac914cd269de
JohnnyOhms <officialjohn662@gmail.com>
2023-03-24 00:32:59 +0100

    socials icon added

--- src/component/AppBar/AppBar.tsx
+++ src/component/AppBar/AppBar.tsx
@@ -4,6 +4,9 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import RssFeedIcon from "@mui/icons-material/RssFeed";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 const AppNavBar = () => {
   return (
@@ -30,6 +33,18 @@ const AppNavBar = () => {
               connect
             </button>
           </form>
+
+          <div className="social">
+            <a href="">
+              <GitHubIcon />
+            </a>
+            <a href="">
+              <LinkedInIcon />
+            </a>
+            <a href="">
+              <TwitterIcon />
+            </a>
+          </div>
         </Toolbar>
       </AppBar>
     </Box>
