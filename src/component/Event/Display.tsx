e03baa9e0c60ec1c28fb2fa003f5751fd92fe96b
JohnnyOhms <officialjohn662@gmail.com>
2023-03-29 00:13:06 +0100

    get data from emit section

--- src/component/Event/Display.tsx
+++ src/component/Event/Display.tsx
@@ -1,10 +1,19 @@
+import { useEffect, useState, useRef } from "react";
 import { Stack } from "@mui/material";
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
+import { useAppSelector } from "../../hooks/hooks";
+import { emitEvents } from "../../type";
 
 const Display = () => {
   const value = 12345;
+  const event = useAppSelector((state) => state.main.emitEvent);
+  const [emitData, setEmitData] = useState<emitEvents>([]);
+  const scrollRef = useRef();
 
+  useEffect(() => {
+    setEmitData(event);
+  }, [event]);
   return (
     <div className="display-section">
       <div className="btn-emit">
@@ -20,35 +29,42 @@ const Display = () => {
           display: "flex",
         }}
       >
-        <div className="align-mssg">
-          <span
-            style={{
-              visibility: "hidden",
-            }}
-          >
-            aligned_hidden
-          </span>
+        {emitData &&
+          emitData.map((item, index) => {
+            return (
+              <>
+                <div className="align-mssg">
+                  <span
+                    style={{
+                      visibility: "hidden",
+                    }}
+                  >
+                    aligned_hidden
+                  </span>
+                  <div className="display-mssg" key={index}>
+                    <Stack sx={{ height: "70px", padding: "4px 7px" }}>
+                      <p>{Date.now()}</p>
+                      <p>{item.name}</p>
+                      <p>{item.data}</p>
+                    </Stack>
+                    <Stack>
+                      <CodeMirror
+                        value={item.content}
+                        height="150px"
+                        extensions={[javascript({ jsx: true })]}
+                        theme="dark"
+                        readOnly={true}
+                      />
+                    </Stack>
+                  </div>
+                </div>
+              </>
+            );
+          })}
 
-          <div className="display-mssg">
-            <Stack sx={{ height: "70px", padding: "4px 7px" }}>
-              <p>23 thursday march</p>
-              <p>event: event</p>
-              <p>Data: Data</p>
-            </Stack>
-            <Stack>
-              <CodeMirror
-                value={JSON.stringify(value)}
-                height="150px"
-                extensions={[javascript({ jsx: true })]}
-                theme="dark"
-                readOnly={true}
-              />
-            </Stack>
-          </div>
-        </div>
         {/*  */}
 
-        <div className="align-mssg" style={{ flexDirection: "row-reverse" }}>
+        {/* <div className="align-mssg" style={{ flexDirection: "row-reverse" }}>
           <span
             style={{
               visibility: "hidden",
@@ -73,7 +89,7 @@ const Display = () => {
               />
             </Stack>
           </div>
-        </div>
+        </div> */}
 
         {/*  */}
       </Stack>
